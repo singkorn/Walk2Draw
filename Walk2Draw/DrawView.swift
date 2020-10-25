bb0f90ea5e2230b24e545adfd72aa79775feff31
Singkorn Dhepyasuvan <singkorn@gmail.com>
2020-10-25 22:35:35 +0700

    Initiated DrawView with MapKit

---
+++ Walk2Draw/DrawView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawView.swift
+//  Walk2Draw
+//
+//  Created by Singkorn Dhepyasuvan on 25/10/2563 BE.
+//
+
+import UIKit
+import MapKit
+
+class DrawView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
